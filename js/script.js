727ece991247c4313e7c686c87199f4de8905cca
christopherzito <christopherzito11@gmail.com>
2021-11-22 13:23:34 +0100

    update end

--- js/script.js
+++ js/script.js
@@ -3,6 +3,19 @@ var app = new Vue(
         el:"#container",
         data:{
             //dati
+            email: [],
         },
+        mounted() {
+            //ciclo for per la creazione di più elementi(e-mail)
+            for(let i = 0; i < 10; i++){
+                const self = this;
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(function(component) {   
+                    //push nell'array email dell'elemento creato                 
+                    self.email.push(component.data.response);
+                });
+            }
+        }
     }
 );
